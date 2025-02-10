c152a1cd00568d72620b82f8c5b0ca6ef7f1d131
Sandro Pizarro <sandro_pizarro@hotmail.com>
2025-02-09 21:55:49 -0300

    Actualizacion de index y app

--- app.js
+++ app.js
@@ -1,1 +1,63 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo() {
+    let nombre = document.getElementById("amigo").value;
+
+    if (nombre !== "") {
+        amigos.push(nombre);
+    } else {
+        alert("Ingrese un valor correcto");
+    }  
+    console.log(amigos);
+    limpiarCaja();
+    actualizarListaDeAmigos();
+}
+
+function limpiarCaja() {
+    document.getElementById("amigo").value = '';
+}
+
+function actualizarListaDeAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += `<li>${amigos[i]}</li>`
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No existen amigos para sortear");
+    } else {
+        let numeroSorteo = Math.floor(Math.random()*amigos.length);
+        nombreAmigoSecreto("h2", `Su amigo secreto es ${amigos[numeroSorteo]}`)
+    }
+    limpiarListaDeAmigos();
+    frenoJuego();
+    return;
+}
+
+function nombreAmigoSecreto(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
+
+function limpiarListaDeAmigos() {
+    let limpieza = document.getElementById("listaAmigos");
+    limpieza.innerHTML = "";
+}
+
+function frenoJuego() {
+    let botonReinicio = document.getElementById("reinicio")
+    if (botonReinicio) {
+        botonReinicio.disabled = true;
+    } else {
+        console.log("boton no encontrado");
+    }
+}
+
+
+
